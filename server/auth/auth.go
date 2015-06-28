b779dd69742ef67fe6ca9abaecdab9f777921d1b
Francesco Furiani <furio@suppaman>
2015-06-28 23:10:06 +0200

    Godeps

---
+++ server/auth/auth.go
@@ -0,0 +1,24 @@
+package auth
+
+import (
+	"log"
+	"net/http"
+)
+
+type Middleware struct {
+	moose bool
+}
+
+// Middleware is a struct that has a ServeHTTP method
+func NewMiddleware() *Middleware {
+	return &Middleware{true}
+}
+
+// The middleware handler
+func (l *Middleware) ServeHTTP(w http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
+	// Log moose status
+	log.Printf("MOOSE: %v\n", l.moose)
+
+	// Call the next middleware handler
+	next(w, req)
+}
